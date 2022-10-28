d6f9ec483679b669f41024cc5fb45e4006e2007e
Chethan Gowda <chethanrgowda38@gmail.com>
2022-10-28 12:37:46 +0530

    Cd and Entities

--- Petzey/Model/Entities/Pet.cs
+++ Petzey/Model/Entities/Pet.cs
@@ -8,5 +8,6 @@
         public DateTime DateOfBirth { get; set; }
         public int Age { get; set; }
         public string BloodGroup { get; set; }
+        public PetOwner PetOwner { get; set; }
     }
 }
