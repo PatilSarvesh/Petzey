0c1fe8d2e82a20beca7a7e7715365145b52a74ef
PSarvesh <PSarvesh@OPT-LAP-0651>
2022-10-28 13:40:37 +0530

    Some changes

---
+++ Petzey/Migrations/20221028074348_inittee.cs
@@ -0,0 +1,533 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Petzey.Migrations
+{
+    public partial class inittee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PetOwners_Appointments_AppointmentId",
+                table: "PetOwners");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PetOwners_Pets_PetId",
+                table: "PetOwners");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_Symptoms_SymptomId",
+                table: "Pets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vitals_Appointments_AppointmentId",
+                table: "Vitals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Vitals_AppointmentId",
+                table: "Vitals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pets_SymptomId",
+                table: "Pets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PetOwners_AppointmentId",
+                table: "PetOwners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PetOwners_PetId",
+                table: "PetOwners");
+
+            migrationBuilder.DropColumn(
+                name: "AppointmentId",
+                table: "Vitals");
+
+            migrationBuilder.DropColumn(
+                name: "SymptomId",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "AppointmentId",
+                table: "PetOwners");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tests",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Symptoms",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AppointmentId",
+                table: "Symptoms",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SymptomName",
+                table: "Symptoms",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "species",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "petName",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BloodGroup",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PetOwnerId",
+                table: "Pets",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ParentName",
+                table: "PetOwners",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "PetOwners",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "PetOwners",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Medicines",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Speciality",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MobileNo",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Clinics",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClinicAddress",
+                table: "Clinics",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReasonForVisit",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PetId",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SymptomId",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Symptoms_AppointmentId",
+                table: "Symptoms",
+                column: "AppointmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_PetOwnerId",
+                table: "Pets",
+                column: "PetOwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_PetId",
+                table: "Appointments",
+                column: "PetId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Pets_PetId",
+                table: "Appointments",
+                column: "PetId",
+                principalTable: "Pets",
+                principalColumn: "PetId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_PetOwners_PetOwnerId",
+                table: "Pets",
+                column: "PetOwnerId",
+                principalTable: "PetOwners",
+                principalColumn: "PetOwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Symptoms_Appointments_AppointmentId",
+                table: "Symptoms",
+                column: "AppointmentId",
+                principalTable: "Appointments",
+                principalColumn: "AppointmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Pets_PetId",
+                table: "Appointments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_PetOwners_PetOwnerId",
+                table: "Pets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Symptoms_Appointments_AppointmentId",
+                table: "Symptoms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Symptoms_AppointmentId",
+                table: "Symptoms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pets_PetOwnerId",
+                table: "Pets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_PetId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "AppointmentId",
+                table: "Symptoms");
+
+            migrationBuilder.DropColumn(
+                name: "SymptomName",
+                table: "Symptoms");
+
+            migrationBuilder.DropColumn(
+                name: "PetOwnerId",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "PetId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "SymptomId",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AppointmentId",
+                table: "Vitals",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tests",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Symptoms",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "species",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "petName",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BloodGroup",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SymptomId",
+                table: "Pets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ParentName",
+                table: "PetOwners",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "PetOwners",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "PetOwners",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AppointmentId",
+                table: "PetOwners",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Medicines",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Speciality",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MobileNo",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Clinics",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClinicAddress",
+                table: "Clinics",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReasonForVisit",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vitals_AppointmentId",
+                table: "Vitals",
+                column: "AppointmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_SymptomId",
+                table: "Pets",
+                column: "SymptomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PetOwners_AppointmentId",
+                table: "PetOwners",
+                column: "AppointmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PetOwners_PetId",
+                table: "PetOwners",
+                column: "PetId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PetOwners_Appointments_AppointmentId",
+                table: "PetOwners",
+                column: "AppointmentId",
+                principalTable: "Appointments",
+                principalColumn: "AppointmentId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PetOwners_Pets_PetId",
+                table: "PetOwners",
+                column: "PetId",
+                principalTable: "Pets",
+                principalColumn: "PetId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_Symptoms_SymptomId",
+                table: "Pets",
+                column: "SymptomId",
+                principalTable: "Symptoms",
+                principalColumn: "SymptomId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vitals_Appointments_AppointmentId",
+                table: "Vitals",
+                column: "AppointmentId",
+                principalTable: "Appointments",
+                principalColumn: "AppointmentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
