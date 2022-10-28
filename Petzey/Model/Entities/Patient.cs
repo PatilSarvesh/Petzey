cf895b82acfbe2e8390d48d6b2b33da3359ef9a0
Chethan Gowda <chethanrgowda38@gmail.com>
2022-10-28 13:23:47 +0530

    CD Changes

---
+++ Petzey/Model/Entities/Patient.cs
@@ -0,0 +1,15 @@
+﻿namespace Petzey.Model.Entities
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public Appointment Appointment { get; set; }
+        public int AppointmentId { get; set; }
+        public Vital Vital { get; set; }
+        public int VitalId { get; set; }
+        public Test Test { get; set; }
+        public int TestId { get; set; }
+        public List<Prescription> Prescription { get; set; }
+        public int PrescriptionId { get; set; }
+    }
+}
