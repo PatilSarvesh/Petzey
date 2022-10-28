3c41aa11c969eecbf5e380ebf5c3653de2716105
Chethan Gowda <chethanrgowda38@gmail.com>
2022-10-28 12:24:30 +0530

    Added new controller api

---
+++ Petzey/Controllers/api.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Petzey.Controllers
+{
+    public class api : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
