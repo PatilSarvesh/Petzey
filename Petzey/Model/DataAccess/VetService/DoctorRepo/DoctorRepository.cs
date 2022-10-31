e5e871f756b4c1252ac6c3461a23d7cd2a2804da
SubramanyaSwaroop <swaroop.subramanya25@gmail.com>
2022-10-31 10:53:11 +0530

    arsda

--- Petzey/Model/DataAccess/VetService/DoctorRepo/DoctorRepository.cs
+++ Petzey/Model/DataAccess/VetService/DoctorRepo/DoctorRepository.cs
@@ -10,6 +10,8 @@ namespace Petzey.Model.DataAccess.VetService.DoctorRepo
         public void AddDoctor(Doctor doctor)
         {
             db.Doctors.Add(doctor);
+
+          
             db.SaveChanges();
         }
 
